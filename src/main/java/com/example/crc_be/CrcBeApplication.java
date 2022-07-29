256058ffddb8c45b6a2cf7c12dfa1006114420d7
Marek Čermák <marek.cermi@gmail.com>
2022-07-29 21:41:57 +0200

    Initial commit

---
+++ src/main/java/com/example/crc_be/CrcBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.crc_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrcBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrcBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crc_be/CrcBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crc_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrcBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
